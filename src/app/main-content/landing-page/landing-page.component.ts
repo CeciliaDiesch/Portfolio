c5196f30d160a74f77f77a880643c54cd193f7a2
CeciliaDiesch <ceciliadiesch@yahoo.com>
2025-06-26 15:55:31 +0200

    responsiveness large screen

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
@@ -11,36 +11,6 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent implements AfterViewInit {
+export class LandingPageComponent{
   isGitHover = false;
-  @ViewChild('tickerBar', { static: false }) tickerBar!: ElementRef<HTMLDivElement>;
-
-  
-  /**
-   * Lifecycle hook that runs after the view has been fully initialized.
-   * Starts the smooth scrolling ticker animation.
-   */
-  ngAfterViewInit() {
-    this.startSmoothTicker();
-  }
-  
-  /**
-   * Creates a continuously scrolling ticker effect by updating the scroll position of the ticker bar.
-   * Resets the scroll position when half the content has been scrolled to create a seamless loop.
-   */
-  private startSmoothTicker() {
-    const bar = this.tickerBar.nativeElement;
-    const resetPoint = bar.scrollWidth / 2;
-    let pos = 0;
-    const speed = 0.5;
-    const step = () => {
-      pos += speed;
-      if (pos >= resetPoint) {
-        pos = 0;
-      }
-      bar.scrollLeft = pos;
-      requestAnimationFrame(step);
-    };
-    requestAnimationFrame(step);
-  }
 }
